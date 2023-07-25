424cb1c92158ff5b74a9b29a07bf1b227725a654
RachelBra <racheliur10@gmail.com>
2023-07-25 20:14:57 +0300

    first

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BasicFilterDemo from './components/DataTable';
+import "primereact/resources/themes/lara-light-indigo/theme.css";  
+// import {BasicFilterDemo} from './components/DataTable'
+
+function App() {
+  return (
+    <BasicFilterDemo></BasicFilterDemo>
+  );
+}
+
+export default App;
